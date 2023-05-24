76029ed3647f2b58e94c3da6bbe346f7866fd10b
Gris-D <amate.grey@gmail.com>
2023-05-25 01:28:04 +0300

    rename

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,8 +4,9 @@ import s from './header.module.css'
 const Header = () => {
   return (
     <header className={s.header}>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjFMReJvcQ0Z1pD2h1FXmwqJT8BSii9W8FlQ&usqp=CAU" />
-    </header>
+      <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjFMReJvcQ0Z1pD2h1FXmwqJT8BSii9W8FlQ&usqp=CAU" />
+			<h6 className={s.name}>Spidoznie kozyavki</h6></div>
+		</header>
   );
 };
 
